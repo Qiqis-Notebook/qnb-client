48490eab3773cc52fda3bf237dd8ab460d281f7c
Kennie-L <62367598+Kennie-L@users.noreply.github.com>
2023-11-12 18:41:50 +0800

    TailwindCSS + DaisyUI

---
+++ src/main/preload.ts
@@ -0,0 +1,31 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// Disable no-unused-vars, broken for spread args
+/* eslint no-unused-vars: off */
+import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
+
+export type Channels = "ipc-bridge";
+
+const electronHandler = {
+  ipcRenderer: {
+    sendMessage(channel: Channels, ...args: unknown[]) {
+      ipcRenderer.send(channel, ...args);
+    },
+    on(channel: Channels, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => {
+        ipcRenderer.removeListener(channel, subscription);
+      };
+    },
+    once(channel: Channels, func: (...args: unknown[]) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+  },
+};
+
+contextBridge.exposeInMainWorld("electron", electronHandler);
+
+export type ElectronHandler = typeof electronHandler;
